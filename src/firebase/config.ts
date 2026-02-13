836bd01b6d03a667553102b3e214249c9c9e9c43
Lucifer War <warlucifer87@gmail.com>
2026-02-13 06:55:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "gtpro-f9b5c",
+  "appId": "1:665820713624:web:247b15447bb6d683d5b6ed",
+  "apiKey": "AIzaSyDnMitSGFltbAE0RQBRDAQl7mhfpprvRL8",
+  "authDomain": "gtpro-f9b5c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "665820713624"
+};
