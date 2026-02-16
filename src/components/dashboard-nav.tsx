27b3aae7d6377a121222e08c83c8bdb69b1b6863
Lucifer War <warlucifer87@gmail.com>
2026-02-16 09:18:27 +0000

    remove (_for element <SlotClone>_)

--- src/components/dashboard-nav.tsx
+++ src/components/dashboard-nav.tsx
@@ -151,12 +151,6 @@ export function DashboardNav() {
           >
             <DropdownMenuLabel>My Account</DropdownMenuLabel>
             <DropdownMenuSeparator />
-            <DropdownMenuItem asChild>
-              <Link href="/dashboard/settings">
-                <Settings className="mr-2 h-4 w-4" />
-                <span>Settings</span>
-              </Link>
-            </DropdownMenuItem>
              <DropdownMenuItem asChild>
               <Link href="/dashboard/settings#help">
                 <HelpCircle className="mr-2 h-4 w-4" />
