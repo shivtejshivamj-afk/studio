85343ccdcd328b649e64e4116050c3fb3745644d
Lucifer War <warlucifer87@gmail.com>
2026-03-05 07:07:11 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@/

--- src/app/dashboard/payments/page.tsx
+++ src/app/dashboard/payments/page.tsx
@@ -42,7 +42,7 @@ import {
   DropdownMenuItem,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from '@/dropdown-menu';
+} from '@/components/ui/dropdown-menu';
 import {
   Dialog,
   DialogContent,
